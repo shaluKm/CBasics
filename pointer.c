e7cd7eaae06b4747409a9879597a4a3ce07f8cb0
shaluKm <shalu.dakshana16@gmail.com>
2023-02-10 17:16:43 +0530

    pointer

---
+++ pointer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+
+
+int age;
+int *address = &age;
+
+char name[7]="Meghan";
+
+*address = 37;
+
+
+printf("%c\n", *name);
+
+printf("%p\n", address);
+printf("%u\n", *address);
+printf("%d\n", age);
+
+
+
+
+
+
+return 0;
+}
