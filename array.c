50aa6eff60427f8eca4a36321815a6dd1b609f78
shaluKm <shalu.dakshana16@gmail.com>
2023-02-10 16:25:36 +0530

    array

---
+++ array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int i;
+
+int age[10]={1,2,3,4,5,6,7,8,9,10};
+for(i=0;i<10;i++)
+printf("%d\n",age[i]);
+
+int price[3]={10,20,30};
+
+for(i=0;i<3;i++){
+printf("%d\t",price[i]);
+}
+
+return 0;
+}
